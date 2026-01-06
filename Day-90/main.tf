8e487eea96535c4efb88211d5ed7d17d1bedce53
gauravhalnawar1011 <gauravhalnawar1011@gmail.com>
2026-01-06 11:41:31 +0530

    s3 bucket test for the github action useing oidc

---
+++ Day-90/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+
+  backend "s3" {
+    bucket        = "space9-terraform-bucket-v1"
+    key           = "dev/terraform.tfstate"
+    region        = "ap-south-1"
+    encrypt       = true
+    use_lockfile  = true
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "test_backend" {
+  bucket = "test-remote-backend-${random_string.bucket_suffix.result}"
+
+  tags = {
+    Name        = "Test Backend Bucket"
+    Environment = "dev"
+  }
+}
